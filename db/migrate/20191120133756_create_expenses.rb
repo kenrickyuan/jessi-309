b0f3d69229eb4fad9bee0b02b3a06a53054f5d3c
Kristina Head <kristina_head@icloud.com>
2019-11-20 14:40:24 +0100

    expense model and db migration created

---
+++ db/migrate/20191120133756_create_expenses.rb
@@ -0,0 +1,12 @@
+class CreateExpenses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :expenses do |t|
+      t.references :event, foreign_key: true
+      t.string :description
+      t.monetize :amount
+      t.references :guest, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_141458) do
+ActiveRecord::Schema.define(version: 2019_11_20_104115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 2019_11_19_141458) do
     t.date "start_time"
     t.date "end_time"
     t.integer "guests"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "guests", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
