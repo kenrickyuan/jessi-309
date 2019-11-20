321a2027599bd7cfd5b17ed1959a036ef9d1b5f0
Antonia Lütz <antonialuetz@gmx.de>
2019-11-20 11:47:02 +0100

    added Guests Model and Controller

---
+++ app/controllers/guests_controller.rb
@@ -0,0 +1,19 @@
+class GuestsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def createedit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
