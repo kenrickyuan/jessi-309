ebdfaa1b044b479023d6eb64248da9c0bac20f52
Paul Giganti <gigantipaul@gmail.com>
2019-11-21 15:15:58 +0100

    datetimepicker added and form polished

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'uglifier'
 gem 'webpacker'
 gem 'httparty'
 gem 'monetize'
+gem 'datetime_picker_input'
 
 group :development do
   gem 'web-console', '>= 3.3.0'
---
+++ app/javascript/plugins/datepicker.js
@@ -0,0 +1,14 @@
+import datetimepicker from "jquery-datetimepicker"
+
+const iniDatePicker = () => {
+  $('.picker').datetimepicker({
+    timepicker: true,
+    datepicker: true,
+    format: 'Y-m-d H:i',
+    hours12: false,
+    step: 30,
+    yearStart: 2019,
+    yearEnd: 2021
+  })
+}
+export default iniDatePicker
