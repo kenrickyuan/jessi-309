97e2f7c0b343ce69ef9a96a6aac024c3bd07a086
Kristina Head <kristina_head@icloud.com>
2019-11-24 11:37:31 +0100

    Added settled logic and fixed bug in view that duplicated transactions

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -35,8 +35,8 @@ class ExpensesController < ApplicationController
     filter_by_description if params[:search] && params[:search][:description].present?
     filter_by_guest if params[:search] && params[:search][:guest].present?
     # For testing
-    @guests_by_id = allocate_guests
-    calculate_balance
+    # @guests_by_id = allocate_guests
+    # calculate_balance
     # For testing ^^^
     @expenses
   end
@@ -44,6 +44,7 @@ class ExpensesController < ApplicationController
   def show
     set_expense
     set_transactions
+    settle_transactions
   end
 
   def destroy
@@ -66,20 +67,41 @@ class ExpensesController < ApplicationController
     @event = Event.find(params[:event_id])
   end
 
-  def set_guests
-    @guests = @event.guests
-  end
+  # def set_guests
+  #   @guests = @event.guests
+  # end
 
   def set_transactions
-    @transactions = {}
-    @event.expenses.each do |expense|
-      @transactions[expense.id] = []
-      Transaction.where(expense_id: expense.id).each do |transaction|
-        @transactions[expense.id] << transaction
-      end
-      @transactions.delete(expense.id) if @transactions[expense.id].empty?
+    @transactions = []
+    Transaction.where(expense_id: @expense.id).each do |transaction|
+      @transactions << transaction
     end
     @transactions
+  end
+
+  def settle_transactions
+    @transactions_hash = {}
+    @transactions.each do |transaction|
+      if transaction.is_debt == false
+        if (@transactions_hash.key?(transaction.payer))
+          @transactions_hash[transaction.payer] += transaction.amount
+        else
+          @transactions_hash[transaction.payer] = 0
+          @transactions_hash[transaction.payer] += transaction.amount
+        end
+      end
+    end
+    @transactions.each do |transaction|
+      if transaction.is_debt == true
+        if (@transactions_hash.key?(transaction.payer))
+          @transactions_hash[transaction.payer] -= transaction.amount
+        else
+          @transactions_hash[transaction.payer] = 0
+          @transactions_hash[transaction.payer] -= transaction.amount
+        end
+      end
+      @transactions_hash
+    end
   end
 
   def filter_by_description
@@ -90,32 +112,32 @@ class ExpensesController < ApplicationController
     @expenses = @expenses.where(guest: params[:search][:guest])
   end
 
-  def allocate_guests
-    set_guests
-    @guests_by_id = {}
-    @guests.each do |guest|
-      @guests_by_id[guest.id] = guest.name
-    end
-    @guests_by_id
-  end
+  # def allocate_guests
+  #   set_guests
+  #   @guests_by_id = {}
+  #   @guests.each do |guest|
+  #     @guests_by_id[guest.id] = guest.name
+  #   end
+  #   @guests_by_id
+  # end
 
-  def calculate_balance
-    @balance_per_guest = {}
-    set_guests
-    @guests.each do |guest|
-      @balance_per_guest[guest.id] = 0
-    end
-    @expenses.each do |expense|
-      balance = expense.amount / @guests.size
-      @balance_per_guest[expense.guest_id] += balance
-      @guests.each do |guest|
-        next if guest.id == expense.guest_id
+  # def calculate_balance
+  #   @balance_per_guest = {}
+  #   set_guests
+  #   @guests.each do |guest|
+  #     @balance_per_guest[guest.id] = 0
+  #   end
+  #   @expenses.each do |expense|
+  #     balance = expense.amount / @guests.size
+  #     @balance_per_guest[expense.guest_id] += balance
+  #     @guests.each do |guest|
+  #       next if guest.id == expense.guest_id
 
-        @balance_per_guest[guest.id] -= balance
-      end
-    end
-    @balance_per_guest
-  end
+  #       @balance_per_guest[guest.id] -= balance
+  #     end
+  #   end
+  #   @balance_per_guest
+  # end
 
   # def calculate_outstanding_balance
   #   positive_balances = []
