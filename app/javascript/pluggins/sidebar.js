b701e76a651d518893bff13780f19330a6be8abe
Kenrick Yuan <kenrickyuan@gmail.com>
2019-11-26 15:13:53 +0100

    more fixes

--- app/javascript/pluggins/sidebar.js
+++ app/javascript/pluggins/sidebar.js
@@ -1,17 +1,16 @@
 // /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-  const openNav = () => {
-    console.log('open', document.getElementById("mySidebar"))
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
+  // const openNav = () => {
+  //   console.log('open', document.getElementById("mySidebar"))
+  //   document.getElementById("mySidebar").style.width = "250px";
+  //   document.getElementById("main").style.marginLeft = "250px";
+  // }
 
 
-
- const openSideBar = () => {
-  const openNavBtn = document.querySelector('.openbtn');
- if (closeSideBar.style.width == '250px') return;
-   openNavBtn.addEventListener('click', openNav);
- };
+ // const openSideBar = () => {
+ //  const openNavBtn = document.querySelector('.openbtn');
+ // if (closeSideBar.style.width == '250px') return;
+ //   openNavBtn.addEventListener('click', openNav);
+ // };
 
 // console.log("hellofromoutside")
 
