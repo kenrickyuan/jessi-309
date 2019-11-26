4764f9512687707df13f251554453a872639e143
Paul Giganti <gigantipaul@gmail.com>
2019-11-26 14:46:24 +0100

    chilled lion added but inactive for the meantime

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@ import iniDatePicker from "../plugins/datepicker"
 import initMultipleChoices from "../pluggins/choices";
 import initMultipleGuests from "../pluggins/guests";
 import StartTextAnimation from "../pluggins/typewriter";
+import "../pluggins/easteregg";
 
 iniDatePicker();
 initMultipleChoices();
