20f94c5cb625971f4fb23e127ac3734d83a0b959
Antonia Lütz <antonialuetz@gmx.de>
2019-11-27 19:27:24 +0100

    Sidebar done, needs stylefixing

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,7 +5,7 @@ import iniDatePicker from "../plugins/datepicker";
 import initMultipleChoices from "../pluggins/choices";
 import initMultipleGuests from "../pluggins/guests";
 import TypeWriter from "../pluggins/typewriter";
-import initSidebar from "../pluggins/sidebar";
+// import initSidebar from "../pluggins/sidebar";
 // import "../pluggins/sidebarclose";
 
 
@@ -13,4 +13,4 @@ iniDatePicker();
 initMultipleChoices();
 initMultipleGuests();
 console.log(new TypeWriter());
-initSidebar();
+// initSidebar();
